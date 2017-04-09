af6551e1c693f0ce79e54d90479a8b5179e76b0e
Jesus Guerra <jguerrix@gmail.com>
2017-04-09 01:57:31 -0700

    Add new registration endpoint

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,9 @@
 
 const express = require('express')
 const api = express.Router()
+const authConstroller = require('../controllers/auth')
+
+api.post('/facebooksignUp/:facebookToken', authConstroller.signUpWithFacebook)
 
 api.get('/', (req, res) => {
   res.send({ message: 'Hello Wold!' })
