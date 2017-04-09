7753f6dc2ba6845424837ea2d9c3c2ff6ecc2eca
Jesus Guerra <jguerrix@gmail.com>
2017-04-09 01:56:43 -0700

    Create services layer

---
+++ services/index.js
@@ -0,0 +1,14 @@
+'use strict'
+
+const jwt = require('jwt-simple')
+const moment = require('moment')
+const config = require('../config')
+
+module.exports = { createToken: function (user) {
+  const payload = {
+    sub: user._id,
+    iat: moment().unix(),
+    exp: moment().add(30, 'days').unix()
+  }
+  return jwt.encode(payload, config.jwtSecretToken)
+}}
