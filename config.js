d1001f5ac9a874d21981c6da3cbef9d8bc8d5b71
Jesus Guerra <jguerrix@gmail.com>
2017-04-09 00:32:59 -0700

    Update mongo db path

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
   port: process.env.PORT || 3000,
-  db: process.env.MONGODB
+  db: process.env.MONGODB_URI
 }
--- index.js
+++ index.js
@@ -7,16 +7,12 @@ const app = require('./app')
 
 const config = require('./config')
 
-app.listen(config.port, () => {
-  console.log(`API REST corriendo en http://localhost:${config.port}`)
+mongoose.connect(config.db, (err, res) => {
+  if (err) {
+    return console.log(`Error al conectar base de datos: ${err}`)
+  }
+  console.log('Conexion a la base de datos exitosa....')
+  app.listen(config.port, () => {
+    console.log(`API REST corriendo en http://localhost:${config.port}`)
+  })
 })
-
-// mongoose.connect(config.db, (err, res) => {
-//   if (err) {
-//     return console.log(`Error al conectar base de datos: ${err}`)
-//   }
-//   console.log('Conexion a la base de datos exitosa....')
-//   app.listen(config.port, () => {
-//     console.log(`API REST corriendo en http://localhost:${config.port}`)
-//   })
-// })
