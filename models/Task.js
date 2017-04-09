dc15180fc25ec1be87879d02e1f4d6abc17e6e3a
Jesus Guerra <jguerrix@gmail.com>
2017-04-09 02:10:01 -0700

    Follow standard JS

--- models/Task.js
+++ models/Task.js
@@ -3,6 +3,9 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
-const TaskSchema = Schema({title: String, description: String})
+const TaskSchema = Schema({
+  title: String,
+  description: String
+})
 
 module.exports = mongoose.model('Task', TaskSchema)
