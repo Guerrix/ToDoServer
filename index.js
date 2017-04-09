d690d165ade66a3f982356af8f0da1c758068460
Jesus Guerra <jguerrix@gmail.com>
2017-04-09 00:26:37 -0700

    Disable monodb
    Just for now

--- index.js
+++ index.js
@@ -7,12 +7,16 @@ const app = require('./app')
 
 const config = require('./config')
 
-mongoose.connect(config.db, (err, res) => {
-  if (err) {
-    return console.log(`Error al conectar base de datos: ${err}`)
-  }
-  console.log('Conexion a la base de datos exitosa....')
-  app.listen(config.port, () => {
-    console.log(`API REST corriendo en http://localhost:${config.port}`)
-  })
+app.listen(config.port, () => {
+  console.log(`API REST corriendo en http://localhost:${config.port}`)
 })
+
+// mongoose.connect(config.db, (err, res) => {
+//   if (err) {
+//     return console.log(`Error al conectar base de datos: ${err}`)
+//   }
+//   console.log('Conexion a la base de datos exitosa....')
+//   app.listen(config.port, () => {
+//     console.log(`API REST corriendo en http://localhost:${config.port}`)
+//   })
+// })
