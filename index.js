547c88a7cb44ad3eec0116f40935696ffbfee273
Jesus Guerra <jguerrix@gmail.com>
2017-04-08 20:29:08 -0700

    Configure index.js

---
+++ index.js
@@ -0,0 +1,18 @@
+'use strict'
+
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const app = express()
+const port = process.env.PORT || 3000
+
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+
+app.get('/', (req, res) => {
+  res.send({ message: 'Hello Wold!' })
+})
+
+app.listen(3000, () => {
+  console.log(`API REST running at http://localhost:${port}`)
+})
